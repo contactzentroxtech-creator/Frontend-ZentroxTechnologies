b027eb74cb7412b120745e9533090c7203f92a6b
contactzentroxtech-creator <contact.zentroxtech@gmail.com>
2026-08-31 14:40:24 +0530

    Update HeroSection.tsx

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
   return (
     <section
       ref={sectionRef}
-      className="relative overflow-hidden bg-white pt-28 pb-16 md:pt-32 md:pb-20 lg:pt-36 lg:pb-24"
+      className="overflow-hidden bg-white pt-28 pb-16 md:pt-32 md:pb-20 lg:pt-36 lg:pb-24"
     >
       {/* Background blobs */}
       <div className="pointer-events-none absolute inset-0 overflow-hidden">
@@ -39,7 +39,7 @@ export default function HeroSection() {
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[500px] w-[500px] rounded-full bg-slate-100/30 blur-[100px]" />
       </div>
 
-      <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 items-center gap-8 lg:grid-cols-2 lg:gap-12">
           {/* Left Content */}
           <motion.div
